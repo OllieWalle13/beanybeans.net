8949307f872b2cb646a84d492534ca7c45856c2a
Bryson <owner@beanybeans.net>
2026-03-28 12:29:32 -0500

    removed example code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import "./main.scss"; // loads all css
-import { doExample } from "./js/example.js"; // loads the function doExample() from example.js
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.innerHTML = "It works!";
-  doExample();
-});
